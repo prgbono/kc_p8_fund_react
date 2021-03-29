71a644744d26eb29f13908cf178d7e06b0ad8c23
Francisco Rios <francisco.riosgalan@keytree.es>
2021-03-29 11:52:46 +0200

    hospital WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-
 // import AdvertsPage from './components/adverts/AdvertsPage';
-import { NodepopLayout } from './components/layout/NodepopLayout.js'
+// import { NodepopLayout } from './components/layout/NodepopLayout.js'
+import { LoginPage } from './components/auth'
 
 
 function App() {
   return (
     <div className="App" id='app'>
         {/* <AdvertsPage/> */}
-        <NodepopLayout />
+        {/* <NodepopLayout /> */}
+        <LoginPage />
     </div>
     
   );
