069c262d9d734205a5d9ca603fee208c283c5cbe
Francisco Ríos <frios@deloitte.es>
2021-04-01 20:16:06 +0200

    hospital WIP 2

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
-import { LoginForm } from './LoginForm'
+import LoginForm from './LoginForm'
 
-// export const LoginPage = () => {
-//   return ( 
-//     <div className="loginPage">
-//       <h1 className="loginPage-title">Log in to Twitter</h1>
-//       <LoginForm />
-//     </div>
-//    );
-// }
+function LoginPage(){
+  return ( 
+    <div className="loginPage">
+      <h1 className="loginPage-title">Log in to NodePop</h1>
+      <LoginForm />
+    </div>
+   );
+}
+
+export default LoginPage;
