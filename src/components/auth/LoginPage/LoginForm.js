0bd8bc41c21a059f469c001c79135462ee6764d4
Francisco Rios <pacoriosgalan@gmail.com>
2021-04-19 18:43:32 +0200

    Login form inputs tied to component state (controlled)

--- src/components/auth/LoginPage/LoginForm.js
+++ src/components/auth/LoginPage/LoginForm.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import FormField from './../../shared/FormField.js'
 import Button  from './../../shared/Button.js'
-
 import './LoginForm.css';
 
 function LoginForm() {
+  const [credentials, setCredentials] = React.useState({
+    username: '',
+    password: '',
+  });
+
+  const handleCredentialsChange = ev => {
+    const newCredentials = {
+      ...credentials, 
+      [ev.target.name]: ev.target.value, //[ev.target.name] === ['username'] or ['password'] new ECMAScript6
+    };
+    setCredentials(newCredentials);
+  }
+
   return (
-    //TODO: Apply styled-components instead of css files
     <form className="loginForm">
       <FormField
         type="text"
-        name="email"
-        label="phone, email or username"
+        name="username"
+        label="Username"
         className="loginForm-field"
+        value= {credentials.username} //tied with component state
+        onChange= {handleCredentialsChange}
       />
       <FormField
         type="password"
         name="password"
         label="password"
         className="loginForm-field"
+        value= {credentials.password} //tied with component state
+        onChange= {handleCredentialsChange}
       />
       <Button type="submit" className="loginForm-submit" variant="primary">
         Log in
