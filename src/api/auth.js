f665ce44a8ad00ae8bfb0216eaed70e03413ea07
Francisco Rios <pacoriosgalan@gmail.com>
2021-04-23 21:20:03 +0200

    Configure Authorization and AxiosClient

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,11 @@
-import axios from './axiosClient';
+import axios, { setAuthorizationHeader } from './axiosClient';
+import storage from './../utils/storage.js';
 
 const authUrl = '/api/auth';
 
-export const login = (credentials) => {
-  return axios.post(`${authUrl}/login`, credentials)
-}
+export const login = async (credentials) => {
+  const accessToken = await axios.post(`${authUrl}/login`, credentials);
+  storage.set(`${process.env.REACT_APP_TOKEN_LS}`, accessToken);
+  setAuthorizationHeader(accessToken);
+  return accessToken;
+};
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
-const axiosClient = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
+const axiosClient = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
 
-axiosClient.interceptors.response.use(response => response.data);
+axiosClient.interceptors.response.use((response) => response.data);
+
+export const setAuthorizationHeader = (token) => {
+  //FIXME: Prettier - Avoid new line when opening array-brackets
+  const { accessToken } = token;
+  axiosClient.defaults.headers.common[
+    'Authorization'
+  ] = `Bearer ${accessToken}`;
+};
 
 export default axiosClient;
