346abfffadee23954b1b824faba0e2376ab13731
Francisco Rios <pacoriosgalan@gmail.com>
2021-04-25 11:41:16 +0200

    Error management

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,18 @@ const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-axiosClient.interceptors.response.use((response) => response.data);
+axiosClient.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (!error.response) {
+      return Promise.reject({ message: error.message });
+    }
+    return Promise.reject({
+      message: error.response.statusText,
+      ...error.response.data,
+    });
+  },
+);
 
 export const setAuthorizationHeader = (token) => {
   //FIXME: Prettier - Avoid new line when opening array-brackets
--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -3,22 +3,28 @@ import LoginForm from './LoginForm';
 import { login } from './../../../api/auth.js';
 
 function LoginPage({ onLogin }) {
+  const [error, setError] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+
   const handleSubmit = async (credentials) => {
     //TODO: Configure try-catch on Axios client
     try {
+      setIsLoading(true);
       await login(credentials);
       onLogin(); //set user logged in App.js state
     } catch (error) {
-      console.log('error: ', error);
+      //TODO: if 401-Unauthorized send the user a more understable message, 'Invalid Credentials'
+      setError(error);
     } finally {
-      // console.log(`Finally del try-catch del login`);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className='loginPage'>
-      <h1 className='loginPage-title'>Log in to NodePop</h1>
-      <LoginForm onSubmit={handleSubmit} />
+    <div>
+      <h1>Log in to NodePop</h1>
+      <LoginForm onSubmit={handleSubmit} isLoading={isLoading} />
+      {error && <div>{error.message}</div>}
     </div>
   );
 }
